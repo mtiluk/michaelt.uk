920f2ca50ec1f5e4f0dd7768f2657a5868e15ad7
Michael <mdtilley04@gmail.com>
2026-07-06 05:47:17 +0100

    quick fix

--- lib/site.config.ts
+++ lib/site.config.ts
@@ -2,6 +2,6 @@ export const siteConfig = {
   name: "Michael Tilley",
   description:
     "Michael Tilley is a final year Computer Science student and research assistant with the School of Computer Science and Digital Technologies at Aston University, Birmingham, UK. His current research interests centre on privacy redesigns of common systems, secure cloud and networks engineering, and embedded and IoT systems. In his free time, he likes to build local server farms and create respective management systems.",
-  url: "https://michaelt.uk/",
+  url: process.env.NEXT_PUBLIC_SITE_URL,
   language: "en",
 };
