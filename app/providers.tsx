9388a2823675557be53f63b711ba5ff02fddbfe5
Michael <mdtilley04@gmail.com>
2026-08-24 12:26:31 +0100

    chore: import fixing

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 import type { ReactNode } from "react";
-import SoundSettingsProvider from "@/components/ui/sound-settings";
+import SoundSettingsProvider from "@/components/providers/sound-settings";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return <SoundSettingsProvider>{children}</SoundSettingsProvider>;
